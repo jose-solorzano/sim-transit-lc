7ec9e91831de50eec3946da330cfce63bed42284
Jose <jsolorzano@logmein.com>
2017-08-05 10:20:50 -0500

    Comment correction.

--- src/main/java/jhs/lc/geom/FluxOrOpacityFunction.java
+++ src/main/java/jhs/lc/geom/FluxOrOpacityFunction.java
@@ -5,8 +5,8 @@ import java.awt.geom.Rectangle2D;
 public interface FluxOrOpacityFunction extends java.io.Serializable {
 	/**
 	 * Objects that emit light return a positive value up to 1, representing flux.
-	 * Occulters return a number between -1 and 0 representing the negative translucence of the occulter. 
-	 * Translucence is 1 minus opacity, or exp(opticalDepth).
+	 * Occulters return a number between -1 and 0 representing the negative transmittance of the occulter. 
+	 * Transmittance is 1 minus opacity, or exp(-opticalDepth).
 	 */
 	public double fluxOrOpacity(double x, double y, double z);
 	
